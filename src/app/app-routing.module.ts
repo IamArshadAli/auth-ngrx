9b966d01d73876c139d2d62aa6e57375a0b5f28e
IamArshadAli <arshadalikaldane@gmail.com>
2024-01-30 19:22:04 +0530

    implemented conditional menubar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './component/home/home.component';
 import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
 import { authGuard } from './guard/auth.guard';
+import { UsersListComponent } from './component/users-list/users-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [authGuard]},
+  { path: 'users', component: UsersListComponent, canActivate: [authGuard]},
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
 ];
